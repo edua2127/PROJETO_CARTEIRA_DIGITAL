b8ede7264b24a6cd4aadd4c1db95aeea5baa6024
edua2127 <magalhaeseduardo91@gmail.com>
2022-11-08 21:30:19 -0300

    estruturacao da pagina home

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import '../styles/home.css'
-
+import style from '../styles/home.module.css'
+import { Input } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '../styles/home.module.css'
 const home = () => {
+
+    const theme = createTheme({
+        status: {
+            danger: '#e53e3e',
+        },
+        palette: {
+            primary: {
+                main: '#ffffff',
+                darker: '#ffffff',
+            },
+        },
+    });
   return (
-    <div>home</div>
+    <div>
+        <header>
+            <h1 className={style.home_logo}>Walle</h1>
+        </header>
+        <main>
+            <section className={style.home_section}>
+                <article>
+                    <ThemeProvider theme={theme}>
+                        <Input variant="standard" label="Valor" color="primary" />
+                    </ThemeProvider>
+
+                </article>
+            </section>
+        </main>
+    </div>
   )
 }
 
 export default home
+
