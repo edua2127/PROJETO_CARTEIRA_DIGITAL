fcf66959512caf0d60a98f2e26193867b41b274c
Changzer <changzer@gmail.com>
2022-11-15 21:49:39 -0300

    next.config.js redireciona a home para login.

--- next.config.js
+++ next.config.js
@@ -5,3 +5,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
+}
